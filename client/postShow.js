d9573d9dacd21180808d9598a44fec9fd5c2245e
mrbigmouth <b92101007@gmail.com>
2015-06-01 14:53:24 +0800

    加上disquz功能

--- client/postShow.js
+++ client/postShow.js
@@ -4,24 +4,33 @@ Template.postShow.helpers({
   }
 });
 
-// Template.disqus.onRendered(function() {
-//   var post = this.data;
-//   var dsq;
-//   if (window.DISQUS) {
-//     window.DISQUS.reset({
-//       reload: true,
-//       config: function() {
-//         this.page.identifier = post.id;
-//         this.page.title = post.title;
-//         this.page.url = window.location.href;
-//       }
-//     });
-//   }
-//   else {
-//     dsq = document.createElement("script");
-//     dsq.type = "text/javascript";
-//     dsq.async = true;
-//     dsq.src = "//.disqus.com/embed.js";
-//     (document.getElementsByTagName("head")[0] || document.getElementsByTagName("body")[0]).appendChild(dsq);
-//   }
-// });
+Template.disqus.onRendered(function() {
+  var post = this.data;
+  var dsq;
+  var disqus_shortname;
+  var disqus_identifier;
+  var disqus_url;
+  var disqus_developer;
+  if (window.DISQUS) {
+    window.DISQUS.reset({
+      reload: true,
+      config: function() {
+        this.page.identifier = post.id;
+        this.page.title = post.title;
+        this.page.url = window.location.href;
+      }
+    });
+  }
+  else {
+    disqus_shortname = 'zhmeteortutorial';
+    disqus_identifier = post;
+    disqus_title = post;
+    disqus_url = window.location.href;
+    disqus_developer = 1;
+    dsq = document.createElement('script');
+    dsq.type = 'text/javascript';
+    dsq.async = true;
+    dsq.src = "//zhmeteortutorial.disqus.com/embed.js";
+    (document.getElementsByTagName("head")[0] || document.getElementsByTagName("body")[0]).appendChild(dsq);
+  }
+});
